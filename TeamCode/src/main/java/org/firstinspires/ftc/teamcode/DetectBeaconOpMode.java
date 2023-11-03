e70e9780f1b64935ade44fd35922da6f69189196
kingKedSha <kedaar.s@gmail.com>
2023-11-03 17:30:27 -0400

    iuwahg;waiey;owg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectBeaconOpMode.java
@@ -0,0 +1,245 @@
+package main.java.org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.*;
+
+import java.util.HashMap;
+
+@Autonomous(name="Detect Red Beacon", group="Auto")
+public class OpenCVOpMode extends LinearOpMode {
+    OpenCvWebcam webcam;
+    DetectBeaconPipeline pipeline;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        pipeline = new DetectBeaconPipeline();
+        webcam.setPipeline(pipeline);
+        webcam.setMillisecondsPermissionTimeout(2500);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+                telemetry.addLine("Error lmao");
+                telemetry.update();
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+        /*
+         * Wait for the user to press start on the Driver Station
+         */
+        waitForStart();
+
+        while (opModeIsActive()) {
+            /*
+             * Send some stats to the telemetry
+             */
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            if(gamepad1.a)
+            {
+                webcam.stopStreaming();
+            }
+
+            sleep(100);
+        }
+    }
+
+    class DetectBeaconPipeline extends OpenCvPipeline {
+        int latest_x, latest_y;
+
+        public DetectBeaconPipeline() {}
+
+        public int[] getLatestCenter () {
+            return new int[]{this.latest_x, this.latest_y};
+        }
+        
+        @Override
+        public Mat processFrame(Mat input) {
+            Mat temp = new Mat();
+            Mat mask = new Mat();
+            Mat res = new Mat();
+
+            Scalar lowerVal = new Scalar(0, 0, 150);
+            Scalar upperVal = new Scalar(100, 100, 255);
+
+            input.copyTo(temp);
+
+            Core.inRange(temp, lowerVal, upperVal, mask);
+
+            Imgproc.erode(mask, mask, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5)));
+            Imgproc.dilate(mask, mask, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5)));
+
+            input.copyTo(res, mask);
+
+            ArrayList<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+
+            Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            double maxArea = -1;
+            int maxAreaIdx = -1;
+
+            for (int i = 0; i < contours.size(); i++) {
+                double area = Imgproc.contourArea(contours.get(i));
+                if (area > maxArea) {
+                    maxArea = area;
+                    maxAreaIdx = i;
+                }
+            }
+
+            if (maxAreaIdx != -1) {
+                Moments mu = Imgproc.moments(contours.get(maxAreaIdx));
+                int centerX = (int) (mu.get_m10() / mu.get_m00());
+                int centerY = (int) (mu.get_m01() / mu.get_m00());
+                this.latest_x = centerX;
+                this.latest_y = centerY;
+                Imgproc.circle(res, new Point(centerX, centerY), 5, new Scalar(0, 0, 255), -1);
+            }
+
+            return res;
+        }
+
+
+        public void getPosition(Mat input) {
+            // CODE FROM LAST YEAR
+            
+            // int[] camValues = new int[3];
+
+            // Mat coneRegion = input.submat(ROI);
+            // camValues[0] = (int) Core.sumElems(coneRegion).val[0] / (int) ROI.area();
+            // camValues[1] = (int) Core.sumElems(coneRegion).val[1] / (int) ROI.area();
+            // camValues[2] = (int) Core.sumElems(coneRegion).val[2] / (int) ROI.area();
+            // String colorString = camValues[0] + ", " + camValues[1] + ", " + camValues[2];
+            // telemetry.addData("Color in RGB: ", colorString);
+
+            // String colorName = MSE(camValues);
+            // telemetry.addData("Closest Color: ", colorName);
+
+            // /**
+            // if (frameCount == 20) {
+            //     if (colorName.equals("orange")) {
+            //         Auton auton = new Auton(1);
+            //     } else if (colorName.equals("teal")) {
+            //         Auton auton = new Auton(2);
+            //     } else {
+            //         Auton auton = new Auton(3);
+            //     }
+            // }
+            // **/
+
+            // coneRegion.release();
+
+            Mat[] regions;
+            for (int i = 0; i < 3; i++) {
+                regions[i] = input.submat(ROIS[i]);
+            }
+            
+            int[] redValues;
+            for (int i = 0; i < 3; i++) {
+                redValues[i] =  (int) Core.sumElems(coneRegion).val[0] / (int) ROIs[i].area();
+            }
+
+            String side;
+            if (redValues[0] > redValues[1] && redVales[0] > redValues[2]) {
+                side = "left";
+            } else if (redValues[1] > redValues[2]) {
+                side = "middle";
+            } else {
+                side = "right";
+            }
+
+            telemetry.addData("The beacon is in the position: ", side);
+
+            for (int i = 0; i < 3; i++) {
+                regions[i].release();
+            }
+        }
+
+        // CODE FROM LAST YEAR
+        
+        // public String MSE(int[] camValues) {
+        //     int[][] coneColorValues = {{255, 165, 0}, {135, 206, 235}, {255, 0, 255}};
+        //     // in order orange, teal, pink ^^^^^^^
+
+        //     int[] diffs = new int[3];
+
+        //     for (int i = 0; i < 3; i++) {
+        //         int rDiff = (int) Math.pow(coneColorValues[i][0] - camValues[0], 2);
+        //         int gDiff = (int) Math.pow(coneColorValues[i][1] - camValues[0], 2);
+        //         int bDiff = (int) Math.pow(coneColorValues[i][2] - camValues[0], 2);
+        //         diffs[i] = rDiff + gDiff + bDiff;
+        //     }
+
+        //     String diffsString = diffs[0] + ", " + diffs[1] + ", " + diffs[2];
+        //     telemetry.addData("diffs: ", diffsString);
+
+        //     if (diffs[1] < diffs[2] && diffs[1] < diffs[0]) { return "teal"; }
+        //     else if (diffs[2] < diffs[0]) { return "pink"; }
+        //     else { return "orange"; }
+        // }
+
+        // public String euclideanDistance(int[] camValues) {
+        //     int[][] coneColorValues = {{255, 165, 0}, {135, 206, 235}, {255, 0, 255}};
+        //     // in order orange, teal, pink ^^^^^^^
+
+        //     double[] diffs = new double[3];
+
+        //     for (int i = 0; i < 3; i++) {
+        //         diffs[i] = Math.sqrt(Math.pow(coneColorValues[i][0] - camValues[0], 2) + Math.pow(coneColorValues[i][1] - camValues[1], 2) + Math.pow(coneColorValues[i][2] - camValues[2], 2));
+        //     }
+
+        //     if (diffs[1] < diffs[2] && diffs[1] < diffs[0]) { return "teal"; }
+        //     else if (diffs[2] < diffs[0]) { return "pink"; }
+        //     else { return "orange"; }
+        // }
+
+        @Override
+        public void onViewportTapped() {
+            viewportPaused = !viewportPaused;
+
+            if(viewportPaused)
+            {
+                webcam.pauseViewport();
+            }
+            else
+            {
+                webcam.resumeViewport();
+            }
+        }
+    }
+}
