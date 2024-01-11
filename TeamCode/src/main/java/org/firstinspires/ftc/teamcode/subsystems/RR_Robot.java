32eee32c2e1f07a0e4c4295bdba57b3f8f6ae0dd
purelyboba <purelyboba@pop-os.localdomain>
2024-01-11 18:05:38 -0500

    teleop done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RR_Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RR_Robot.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class RR_Robot {
@@ -14,11 +16,13 @@ public class RR_Robot {
     public PID pid;
     public DashTelemetry dashTelemetry;
 
+
     public RR_Robot(HardwareMap hardwareMap, Gamepad gamepad) {
         drive = new SampleMecanumDrive(hardwareMap);
         intake = new Intake(hardwareMap);
         lift = new Lift(hardwareMap, gamepad);
         deposit = new Deposit(hardwareMap);
         dashTelemetry = new DashTelemetry();
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Robot {
     public Drive drive;
@@ -13,12 +18,17 @@ public class Robot {
     public PID pid;
     public DashTelemetry dashTelemetry;
 
+    public ServoEx drone;
+
     public Robot(HardwareMap hardwareMap, Gamepad gamepad) {
         drive = new Drive(hardwareMap);
         intake = new Intake(hardwareMap);
 //        lift = new Lift(hardwareMap, gamepad);
-        lift = new LiftPID(hardwareMap);
+//        lift = new LiftPID(hardwareMap);
         servoDeposit = new Deposit(hardwareMap);
         dashTelemetry = new DashTelemetry();
+        drone = new SimpleServo(
+                hardwareMap, "drone", 0, 120, AngleUnit.DEGREES
+        );
     }
 }
